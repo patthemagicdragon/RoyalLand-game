c8b49c8787e378162df87e82231713571bae253a
patthemagicdragon <pt_cfr@hotmail.com>
2019-09-05 12:21:40 +0700

    Add a full game

--- Node2D.gd
+++ Node2D.gd
@@ -1,19 +1,44 @@
 extends Control
 
+var story = "Hi Traveler welcome to RoyalLand , You want to be an %s , "
 var answers = []
-var questions
+var questions = []
+var question_number = 0
 
 func _ready():
-	#print( story % answers )
 	
-	questions.append("Hi Traveler welcome to RoyalLand /nWhat do you want to be a career ?")
-	questions.append("Nice the assit can healing another people an support /n")
-	$DisplayText.text = questions[0]
-	print(questions[0])
+	questions.append("What do you want to be a career ?")
+	questions.append("Do you want to learn the first skill or not?")
+	questions.append("You will searching the ancient for upgrade your first skill , Do you want to travel?")
+	
+	$VBoxContainer/DisplayText.text = questions[0]
 
 
 func _on_TextureButton_pressed():
-	answers = $PlayerInPut
-	print(answers)
-	print(questions[0])
-	$DisplayText.text = questions[0]
+	
+	if question_number < len(questions):
+		# answer the question 
+		answers.append( $PlayerInput.text )
+		print( answers )
+		$VBoxContainer/DisplayText.text = questions[question_number+1]
+		$PlayerInput.text = ""
+	
+	# pause
+	
+	if question_number == 1:
+		# answer the question 
+		answers.append( $PlayerInput.text )
+		print( answers )
+		$VBoxContainer/DisplayText.text = questions[2]
+		$PlayerInput.text = ""
+	
+	# pause
+	
+	if question_number == 2:
+		# answer the question 
+		answers.append( $PlayerInput.text )
+		print( answers )
+		$VBoxContainer/DisplayText.text = ""
+		$PlayerInput.text = ""
+		
+	question_number = question_number + 1
